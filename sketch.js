18010736a5a05288674aaddf1d8566ab0ef7544b
KingChump <babygorillajr@gmail.com>
2017-04-07 22:54:32 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function preload() {
+  luffy = loadImage("ASSets/luffynomouth.png");
+}
+
+function setup() {
+  createCanvas(620, 620);
+  ellipseMode(CORNER);
+}
+
+function draw() {
+  clear()
+  image(luffy, 0, 0);
+  if (keyIsDown(32)) {
+    fill(0);
+    ellipse(230, 300, 70);
+  } else {
+    strokeWeight(4);
+    line(230, 330, 300, 330);
+  }
+}
+
+function keyPressed() {
+  open = !open;
+}
